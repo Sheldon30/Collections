19c727270be951ab3a865e457a0e4dcfada267f9
Sheldon <aneln700@gmail.com>
2023-09-07 17:26:21 +0300

    Final2

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,12 +4,12 @@ public class Game {
     ArrayList<Player> playerList = new ArrayList<>();
 
 
-
-    public void register (Player player) {
+    public void register(Player player) {
         playerList.add(player);
     }
+
     public Player findByName(String playerName) {
-        for (Player player:playerList){
+        for (Player player : playerList) {
             if (player.getName() == playerName) {
                 return player;
             }
@@ -17,18 +17,18 @@ public class Game {
         return null;
     }
 
-    public int round (String playerName1, String playerName2){
+    public int round(String playerName1, String playerName2) {
         Player player1 = findByName(playerName1);
         Player player2 = findByName(playerName2);
 
-        if(player1 == null || player2 == null) {
+        if (player1 == null || player2 == null) {
             throw new NotRegisteredException(
                     "Игрок(и) не зарегестрирован(ы)!"
             );
         }
-        if(player1.getStrength() > player2.getStrength()) {
+        if (player1.getStrength() > player2.getStrength()) {
             return 1;
-        } else if (player1.getStrength()<player2.getStrength()){
+        } else if (player1.getStrength() < player2.getStrength()) {
             return 2;
         } else {
             return 0;
